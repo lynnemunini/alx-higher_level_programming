5b24d76e0bbbbb5086fd88dc8f18f118cb9285ea
lynnemunini <muninilynne65@gmail.com>
2022-05-31 06:38:22 -0700

    Added negative sign to negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+if number < 0:
+    last = -(last)
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif number == 0:
