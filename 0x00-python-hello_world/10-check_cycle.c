62e9cab9c54ec1e402d13da3ad79a0d6a01e8215
lynnemunini <muninilynne65@gmail.com>
2022-05-30 18:54:52 -0700

    Removed pointer

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,17 +8,17 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slowptr "= list;
-	listint_t *fastptr = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
 		return (0);
 
-	while (slowptr && fastptr && fastptr->next)
+	while (slow && fast && fast->next)
 	{
-		slowptr = slowptr->next;
-		fastptr = fastptr->next->next;
-		if (slowptr == fastptr)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
 
