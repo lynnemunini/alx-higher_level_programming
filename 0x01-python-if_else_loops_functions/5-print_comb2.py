83f578f2ed9b68947dd591f5be97581148ac77e8
lynnemunini <muninilynne65@gmail.com>
2022-05-31 08:14:29 -0700

    Edited last digit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{0}".format(i), end=", ")
-    elif i != 99:
-        print(i, end=", ")
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
     else:
         print(i)
 
