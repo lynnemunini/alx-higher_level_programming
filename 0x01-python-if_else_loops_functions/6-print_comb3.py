40eea879c0bfb555169c1715798dbd480ea1530d
lynnemunini <muninilynne65@gmail.com>
2022-05-31 08:43:45 -0700

    Hardcoded the values

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i+1, 10):
-        print("{0}{1}".format(i, j), end=", ")
+        if (i != 8) and (j != 9):
+            print("{0}{1}".format(i, j), end=", ")
+        else:
+            print("{0}{1}".format(i, j))
