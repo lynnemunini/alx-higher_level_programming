8ade0b1002d2cc59c4a18874ae9adc288270d1e0
lynnemunini <muninilynne65@gmail.com>
2022-05-30 18:43:03 -0700

    Added task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if singly linkec list has cycle in it
+ * @list: singly linked list to check
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slowptr = list;
+	listint_t *fastptr = list;
+
+	while (slowptr && fastptr &&fastptr->next)
+	{
+		slowptr = slowptr->next;
+		fastptr = fastptr->next->next;
+
+		if (fastptr == slowptr)
+		{
+			return (1);
+		}
+	}
+
+	if (!list)
+	{
+		return (0);
+	}
+}
