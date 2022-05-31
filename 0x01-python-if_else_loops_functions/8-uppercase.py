2889abdc843349ac30509e9ac766aa94c59580ca
lynnemunini <muninilynne65@gmail.com>
2022-05-31 09:10:39 -0700

    Used format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in str:
         if ord(i) in range(97, 123):
             val = ord(i) - 32
-            print(chr(val), end="")
+            print("{:c}".format(val), end="")
         else:
             print(i, end="")
 
