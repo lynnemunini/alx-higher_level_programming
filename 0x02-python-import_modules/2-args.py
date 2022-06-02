afb469566c283b390adaec72cb8c812a28351c42
lynnemunini <muninilynne65@gmail.com>
2022-06-02 12:44:32 -0700

    List sliced

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("{} argument:".format(len(argv)))
+        print("{} argument:".format(len(argv[1:])))
     elif len(argv) == 0:
-        print("{} arguments.".format(len(argv)))
+        print("{} arguments.".format(len(argv[1:])))
     else:
-        print("{} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv[1:])))
