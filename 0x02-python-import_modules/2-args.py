aa5e3b3a541cc7842dbac39314f537041278f28b
lynnemunini <muninilynne65@gmail.com>
2022-06-02 12:56:42 -0700

    Referenced index of item in argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 1:
+    if len(argv[1:]) == 1:
         print("{} argument:".format(len(argv[1:])))
-    elif len(argv) == 0:
+    elif len(argv[1:]) == 0:
         print("{} arguments.".format(len(argv[1:])))
     else:
         print("{} arguments:".format(len(argv[1:])))
