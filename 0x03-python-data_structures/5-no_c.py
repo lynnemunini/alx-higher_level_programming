9cd3d95e7f02d45fa7353a1935f7830632d17812
lynnemunini <muninilynne65@gmail.com>
2022-06-04 13:26:30 -0700

    Removed trailing white space

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,6 @@
 def no_c(my_string):
     for i in my_string:
         if (i == "c") or (i == "C"):
-            my_string = my_string[:my_string.index(i)] + my_string[my_string.index(i) + 1:]
+            my_string = my_string[:my_string.index(i)] \
+                    + my_string[my_string.index(i) + 1:]
     return my_string
