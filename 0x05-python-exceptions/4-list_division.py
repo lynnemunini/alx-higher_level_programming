e8a2713b13a9f6f38cb398fb2cefa5e4c50dd1be
lynnemunini <muninilynne65@gmail.com>
2022-06-20 15:18:30 -0700

    Added result for when index out of range

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,6 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
             print("division by 0")
         except IndexError:
+            div = 0
             print("out of range")
         finally:
             new_list.append(div)
