4ad738b9fc6ad9317875b7b2739c831a92609fea
lynnemunini <muninilynne65@gmail.com>
2022-06-20 15:20:54 -0700

    Changed loop to use list_length

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(len(my_list_1)):
+    for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
