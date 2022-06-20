e1651bb74f8a86f9bce1ce7d7a7d103ba6945683
lynnemunini <muninilynne65@gmail.com>
2022-06-20 13:09:09 -0700

    Added task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        value = 0
+        for i in my_list[:x]:
+            print("{:d}".format(i), end="")
+            value += 1
+        print()
+        return value
+    except Exception:
+        pass
