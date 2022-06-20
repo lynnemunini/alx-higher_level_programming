ec200f0e3c3e89c02ef1a9163f1cc849a53cc882
lynnemunini <muninilynne65@gmail.com>
2022-06-20 12:43:30 -0700

    Added task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        value = 0
+        for i in my_list[:x]:
+            print("{}".format(i), end="")
+            value += 1
+        print()
+        return value
+    except:
+        print()
