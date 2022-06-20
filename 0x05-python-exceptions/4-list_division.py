7e55c8f93babbdc4f085e2d612216adf8cf17153
lynnemunini <muninilynne65@gmail.com>
2022-06-20 15:26:03 -0700

    Removed trailing blank line

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,6 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
+            div = 0
         except ZeroDivisionError:
             div = 0
             print("division by 0")
