a5bf82a40a4b4d58a729c313b79844fc0bf5ca89
lynnemunini <muninilynne65@gmail.com>
2022-06-20 13:34:20 -0700

    Not raising exception bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     try:
         value = 0
         for i in my_list[:x]:
-            print("{:d}".format(i), end="")
-            value += 1
+            if type(i) != int:
+                pass
+            else:
+                print("{:d}".format(i), end="")
+                value += 1
         print()
         return value
-    except Exception:
-        pass
+    except IndexError:
+        raise
