bdcc9e027d492965beacb7b8c4be185bbfc8ec38
lynnemunini <muninilynne65@gmail.com>
2022-06-21 14:28:05 -0700

    Removed trailing blank lines

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,8 @@
 class Square:
     '''Class Square that defines a square with attribute size'''
     def __init__(self, size):
-        '''The size of a square is crucial for a square, many things depend of it (area computation, etc.). It is important to keep to keep this value private so as to ensure the class builder controls the type and value of this attribute'''
+        '''The size of a square is crucial for a square, many things depend \
+        of it (area computation, etc.). It is important to keep to keep \
+        this value private so as to ensure the class builder controls \
+        the type and value of this attribute'''
         self.__size = size
