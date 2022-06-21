9bdc672a12a5ac3b537748ad951f6ee2b123b959
lynnemunini <muninilynne65@gmail.com>
2022-06-21 14:23:47 -0700

    Added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Class Square that defines a square'''
+
+
+class Square:
+    '''Class Square that defines a square with attribute size'''
+    def __init__(self, size):
+        '''The size of a square is crucial for a square, many things depend of it (area computation, etc.). It is important to keep to keep this value private so as to ensure the class builder controls the type and value of this attribute'''
+        self.__size = size
