ae7258d2a0c7d902928cc4b41be70e5362ed4453
lynnemunini <muninilynne65@gmail.com>
2022-06-21 17:00:42 -0700

    Added checking for int

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,9 @@ class Square:
     @position.setter
     def position(self, value):
         '''Property setter to change the value of position'''
-        if (type(value) != tuple) or (value[0] < 0) or (value[1] < 0):
+        if (type(value) != tuple) or (value[0] < 0) or \
+                (value[1] < 0) or (type(value[0]) != int) or \
+                (type(value[1]) != int):
             raise TypeError("position must be a tuple of \
                         2 positive integers")
         self.__position = value
@@ -50,7 +52,7 @@ class Square:
         if self.__size == 0:
             print()
         for i in range(self.__size):
-            print("_" * self.__position[0], end="")
+            print(" " * self.__position[0], end="")
             for j in range(self.__size):
                 print("#", end="")
             print()
