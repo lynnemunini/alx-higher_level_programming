0ae1ae3132b3f983712e4a2989ac9adc4ade2bcb
lynnemunini <muninilynne65@gmail.com>
2022-06-21 17:09:38 -0700

    Added checking for int

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,6 +54,4 @@ class Square:
         else:
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+                print("#" * self.__size)
