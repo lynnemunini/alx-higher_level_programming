ac86af651716dc996481fe09dc666487406ddd0f
lynnemunini <muninilynne65@gmail.com>
2022-06-24 03:56:58 -0700

    Added function documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+
+"""
+Function that adds 2 integers.
+"""
+
+
 def add_integer(a, b=98):
+
     """
-    Function that adds 2 integers
+    Function that adds 2 integers. a and b must be integers or floats,
+    otherwise TypeError exception is raised with the message
+    a must be an integer or b must be an integer. a and b are also
+    casted into integer if they are float.
     """
-    if isinstance(a, int) == False and isinstance(a, float) == False:
+
+    if isinstance(a, int) is False and isinstance(a, float) is False:
         raise TypeError("a must be an integer")
-    if isinstance(b, int) == False and isinstance(b, float) == False:
+    if isinstance(b, int) is False and isinstance(b, float) is False:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
