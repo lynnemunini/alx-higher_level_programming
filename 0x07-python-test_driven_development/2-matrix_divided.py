77b784249cacc3bc9ea928bacf3f23e8eb9cadff
lynnemunini <muninilynne65@gmail.com>
2022-06-24 04:07:27 -0700

    Created function doc for task 2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Function that divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+
+    """
+    matrix must be a list of lists of integers or floats,
+    otherwise TypeError exception raised with the message matrix
+    must be a matrix (list of lists) of integers/floats.
+    ================================
+    Each row of the matrix must be of the same size, otherwise
+    TypeError exception is raised with the message
+    Each row of the matrix must have the same size.
+    ================================
+    div must be a number (integer or float), otherwise TypeError
+    exception with the message div must be a number
+    ================================
+    div can’t be equal to 0, otherwise ZeroDivisionError exception
+    with the message division by zero
+    ================================
+    Elements of the matrix divided by div and rounded to 2 decimal
+    places
+    """
+
+
