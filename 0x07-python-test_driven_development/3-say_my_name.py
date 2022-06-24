6ee5c8f542fdb169236ebe9b7723603b00ea5ded
lynnemunini <muninilynne65@gmail.com>
2022-06-24 06:23:29 -0700

    Added task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Function that prints My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    
+    """
+    first_name and last_name must be strings otherwise, raise a TypeError
+    exception
+    """
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
