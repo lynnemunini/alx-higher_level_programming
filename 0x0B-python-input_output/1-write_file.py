60a3563f24fb18ff82687d2689371ba8a0baacfd
lynnemunini <muninilynne65@gmail.com>
2022-07-05 11:14:02 -0700

    Added task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that has a function that 
+writes a string to a text file (UTF8) and
+returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    function that writes a string to a text file (UTF8) and returns the number
+    of characters written
+    """
+    with open(filename, encoding="utf-8", mode="w") as f:
+        return f.write(text)
