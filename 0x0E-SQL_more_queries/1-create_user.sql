70c61d57ba45fdc165cb4d2ef52d10c48a972585
lynnemunini <muninilynne65@gmail.com>
2022-08-10 05:22:24 -0700

    script that creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT PROXY
+ON root
+TO user_0d_1@localhost;
+
