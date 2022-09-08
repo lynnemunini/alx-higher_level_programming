582d3b47a291260e557a7cf58dae8ea776c05971
lynnemunini <muninilynne65@gmail.com>
2022-09-08 22:48:06 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   const myArray = [];
-  for (let i = 2; i < (process.argv.length); i++) {
+  for (let i = 2; i < process.argv.length; i++) {
     myArray.push(parseInt(process.argv[i]));
   }
   myArray.sort().reverse();
