4131f9d4fb57be8ba4d9fe8b9825886b90de4541
lynnemunini <muninilynne65@gmail.com>
2022-09-12 17:41:07 +0300

    Added password argument for connecting to databse

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     """
     Execute only if the file is run directly
     """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd="", db=sys.argv[2])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
