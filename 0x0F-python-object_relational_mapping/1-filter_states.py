51f7c3cc0f2a8727533017cd72a5cea39094f2cc
lynnemunini <muninilynne65@gmail.com>
2022-09-13 15:17:25 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,16 +4,16 @@
 Script that lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
 """
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
     """
     Execute only if the file is run directly
     """
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
+                         db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name REGEXP \
             '^[N]' ORDER BY states.id ASC")
