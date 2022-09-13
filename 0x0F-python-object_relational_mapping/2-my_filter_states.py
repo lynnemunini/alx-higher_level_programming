0ab20d031f8d87074743e02a2979643595d862c1
lynnemunini <muninilynne65@gmail.com>
2022-09-13 16:24:15 +0300

    Fix unterminated string literal

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,16 +5,14 @@ Script that takes in an argument and displays
 all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
-import MySQLdb
-import sys
 
 if __name__ == '__main__':
-    """
-    Execute only if the file is run directly
-    """
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '%{}%' ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
