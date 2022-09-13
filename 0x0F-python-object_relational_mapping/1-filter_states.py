9770eb507d96a8f8d35cf66be846655cc2f59f21
lynnemunini <muninilynne65@gmail.com>
2022-09-13 15:11:06 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,10 +4,10 @@
 Script that lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
 
 if __name__ == '__main__':
+    import MySQLdb
+    import sys
     """
     Execute only if the file is run directly
     """
