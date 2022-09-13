02cc8c6e27085916a64d0b4a996b29291fb029ef
lynnemunini <muninilynne65@gmail.com>
2022-09-13 15:13:40 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     Execute only if the file is run directly
     """
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name REGEXP \
             '^[N]' ORDER BY states.id ASC")
