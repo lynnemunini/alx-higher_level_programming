ecbf093289252368678547cd65bd76d02a087f3c
lynnemunini <muninilynne65@gmail.com>
2022-09-13 12:12:39 +0300

    Changes approach of finding states starting with letter N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE \
-            'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name REGEXP \
+            '^[N]' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
