53f20d06dabb111c8e282a0b0cce2c12133625cf
lynnemunini <muninilynne65@gmail.com>
2022-09-13 16:12:47 +0300

    Fix unterminated string literal

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     Execute only if the file is run directly
     """
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd="",
-                         db=sys.argv[2])
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%' ORDER BY states.id ASC".format(sys.argv[3]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%' ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
