3a907b3140411a238c9b2daa5f1f4aae8c4bf1cd
lynnemunini <muninilynne65@gmail.com>
2022-09-15 16:03:00 +0300

    Remove trailing comma

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,15 +11,17 @@ if __name__ == '__main__':
     import sys
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
+                         user=sys.argv[1], passwd="",
+                         db=sys.argv[2], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
             FROM cities INNER JOIN states ON\
             state_id = states.id WHERE states.name=%s\
-            ORDER BY cities.id", (sys.argv[4], ))
+            ORDER BY cities.id", (sys.argv[3], ))
     rows = cur.fetchall()
-    for row in rows:
+    for i, row in enumerate(rows):
+        if i + 1 == len(rows):
+            print(row[1])
         print(row[1], end=", ")
     # Close all cursors
     cur.close()
