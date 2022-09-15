fffe258a9f826d257b66b08f6afbd0f7fb5b4b46
lynnemunini <muninilynne65@gmail.com>
2022-09-15 16:11:38 +0300

    Switch to use RIGHT JOIN

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                          db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM cities INNER JOIN states ON\
+            FROM cities RIGHT JOIN states ON\
             state_id = states.id WHERE states.name=%s\
             ORDER BY cities.id", (sys.argv[4], ))
     rows = cur.fetchall()
