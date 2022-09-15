632ab30374fbbc17b44f254ccaff9944b3062851
lynnemunini <muninilynne65@gmail.com>
2022-09-15 15:24:44 +0300

    All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Script that takes in the name of a state as an
+argument and lists all cities of that state,
+using the database hbtn_0e_4_usa
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd="",
+                         db=sys.argv[2], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities INNER JOIN states ON\
+            state_id = states.id WHERE states.name=%s\
+            ORDER BY cities.id", (sys.argv[3], ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
