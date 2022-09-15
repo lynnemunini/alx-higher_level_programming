80c1858ad7b96e721a53f86a069ad4b0091470e4
lynnemunini <muninilynne65@gmail.com>
2022-09-15 16:04:07 +0300

    Change arguments order

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
     import sys
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd="",
-                         db=sys.argv[2], charset="utf8")
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
             FROM cities INNER JOIN states ON\
             state_id = states.id WHERE states.name=%s\
-            ORDER BY cities.id", (sys.argv[3], ))
+            ORDER BY cities.id", (sys.argv[4], ))
     rows = cur.fetchall()
     for i, row in enumerate(rows):
         if i + 1 == len(rows):
