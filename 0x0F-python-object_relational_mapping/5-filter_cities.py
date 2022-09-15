322ac647f75c3150cc9aace35fe0e997b5904183
lynnemunini <muninilynne65@gmail.com>
2022-09-15 16:31:06 +0300

    Use .format

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,15 +14,15 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
-            FROM cities INNER JOIN states ON\
+    cur.execute("SELECT cities.name\
+            FROM cities RIGHT JOIN states ON\
             state_id = states.id WHERE states.name='{}'\
-            ORDER BY cities.id".format(sys.argv[4]))
+            ORDER BY cities.id".format(sys.argv[4], ))
     rows = cur.fetchall()
     for i, row in enumerate(rows):
         if i + 1 == len(rows):
-            print(row[1])
-        print(row[1], end=", ")
+            print(row[0])
+        print(row[0], end=", ")
     # Close all cursors
     cur.close()
     # Close all databases
