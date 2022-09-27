6c4aa524c38634095c50cccf55590ea217711a65
lynnemunini <muninilynne65@gmail.com>
2022-09-27 06:13:43 -0700

    Remove extra space

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,4 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         data = response.read()
-        print(f"Body response:\n\t - type: {type(data)}\n\t - content: {data}\n\t - utf8 content: {data.decode('utf-8')}")
+        print(f"Body response:\n\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode('utf-8')}")
