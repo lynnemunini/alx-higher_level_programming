2e693376cd10f08d10038c4016305d5be1f11f5f
lynnemunini <muninilynne65@gmail.com>
2022-09-27 12:24:11 -0700

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print(f"Body response:\n\t- type: {type(response.content.decode('utf-8'))}")
+    print(f"\t- content: {response.content.decode('utf-8')}")
