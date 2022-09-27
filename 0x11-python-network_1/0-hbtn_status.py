3aed0ba68a7d7023e35320920e39fe2950b16656
lynnemunini <muninilynne65@gmail.com>
2022-09-27 06:47:45 -0700

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
         data = response.read()
         print(f"Body response:\n\t- type: {type(data)}")
         print("\t- content: {data}")
