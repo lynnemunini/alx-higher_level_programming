161d53166c06c155f2bae692f1f4f5edb3e67cde
lynnemunini <muninilynne65@gmail.com>
2022-09-27 23:35:59 -0700

    script that takes GitHub credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get("https://api.github.com/user",
+                            auth=(username, password))
+    print(response.json().get("id"))
